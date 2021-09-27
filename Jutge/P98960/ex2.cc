dc2822345fbd597205b95fde9d7b3442375c7dca
eur1p3des <72199195+eur1p3des@users.noreply.github.com>
2021-09-27 15:24:32 +0200

    Jutge

---
+++ Jutge/P98960/ex2.cc
@@ -0,0 +1,28 @@
+#include <cctype>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+   // Declare the necessary variables
+   char ch;
+   char low;
+   char up;
+
+   // Ask for  a character
+   //cout << "Enter a character from the alphabet: "; 
+   cin >> ch;
+   
+   // Check if it is uppercase or lowercase and convert it;
+   if(isupper(ch)){
+      low = ch + 32;
+      cout << low << endl;
+   } else if(islower(ch)){
+      up = ch - 32;
+      cout << up << endl;
+   } else {
+      cout << "You have not entered an alphabet character" << endl;
+   }
+}
