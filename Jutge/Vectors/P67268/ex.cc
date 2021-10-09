f21ded3675568938f3958780776dbdeb368cac40
eur1p3des <72199195+eur1p3des@users.noreply.github.com>
2021-10-09 14:14:07 +0200

    Create Vector

---
+++ Jutge/Vectors/P67268/ex.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main (){
+  stack <int> s;
+
+  int n;
+
+  while (cin >> n){
+    while (n--){
+      int p;
+      cin >> p;
+      s.push(p);
+    }
+    if (!s.empty()){
+      cout << ' ' < s.top();
+      s.pop();
+    }
+    while (!s.empty()){
+      cout << ' ' << s.top();
+      s.pop();
+    }
+    cout << endl;
+  }
+}
